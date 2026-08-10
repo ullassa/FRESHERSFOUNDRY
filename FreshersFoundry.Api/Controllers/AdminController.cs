f159f588b58621815ca2462e2326ab344abc8cab
Nithin-ganiga <nithinganiga959@gmail.com>
2026-08-10 18:01:14 +0530

    Fix(AdminController): remove duplicate usings and inject AppDbContext to provide context field

--- FreshersFoundry.Api/Controllers/AdminController.cs
+++ FreshersFoundry.Api/Controllers/AdminController.cs
@@ -6,17 +6,17 @@ using Microsoft.EntityFrameworkCore;
 
 namespace FreshersFoundry.Api.Controllers;
 
-using FreshersFoundry.Api.Data;
-using FreshersFoundry.Api.Models;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-
 [ApiController]
 [Route("api/[controller]")]
 [Authorize(Roles = "Admin")]
 public class AdminController : ControllerBase
 {
+    private readonly AppDbContext context;
+
+    public AdminController(AppDbContext context)
+    {
+        this.context = context;
+    }
     [HttpGet("pending-content")]
     public async Task<IActionResult> PendingContent()
     {
